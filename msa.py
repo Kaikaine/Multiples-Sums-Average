89961b2cf20d032bd0bc59a8daf5a7f2ad486c92
Kairi <kairijb@gmail.com>
2018-04-02 16:08:29 -0500

    first commit

---
+++ msa.py
@@ -0,0 +1,34 @@
+for i in range(1, 1001, 2):
+    print(i)
+    
+# PRINTS ALL ODDS FROM 1-1000
+
+
+
+
+for i in range(5, 1000005, 5):
+    print(i)
+
+# PRINTS MULTIPLES OF 5 FROM 5-1000000
+
+
+
+
+a = [1, 2, 5, 10, 255, 3]
+sum = 0
+for i in range(len(a)):
+    sum+=a[i]
+print(sum)
+
+# PRINTS SUM OF THE ARRAY
+
+
+
+
+a = [1, 2, 5, 10, 255, 3]
+sum = 0
+for i in range(len(a)):
+    sum+=a[i]
+print(sum/len(a))
+
+# PRINTS AVERGAE OF THE ARRAY
